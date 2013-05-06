2545fc819959718e2baec64e845610d680ad1679
James C Russell <james@101ideas.cz>
2013-05-06 18:09:20 +0100

    Formatters docs & specs.

--- lib/logging/formatters.rb
+++ lib/logging/formatters.rb
@@ -2,22 +2,59 @@
 
 module Logging
   module Formatters
+
+    # Default formatter. No colours, just log level, time stamp,
+    # label and the actual log message.
     class Default
+      # Format strings.
+      #
+      # The `single` key is used by {#format_single_message},
+      # whereas `header` is used by {#format_multiple_messages}.
       FORMAT_STRINGS = {
-        single: "%-5s %s -- %s",
-        header: "%-5s %s"
+        single: '%-5s %s -- %s',
+        header: '%-5s %s'
       }
 
-      def timestamp
-        Time.now.strftime('%H:%M:%S')
-      end
-
+      # Format single log message.
+      #
+      # @param level [Symbol] Log level.
+      # @param label [String] Identifier, for instance logs.app.db.
+      # @param message [#to_s] The actual log message.
+      #
+      # @return [String] The log message.
+      #
+      # @see `FORMAT_STRINGS[:single]`
+      #
+      # @api plugin.
+      #
+      # @todo
+      #   There's no documentation for the block yet, it's being
+      #   used only for extending functionality from the subclasses.
+      #   It should be probably refactored, otherwise it will get
+      #   proper documentation.
       def format_single_message(level, label, message, &block)
         args = [label, timestamp, message]
         args = block.call(*args) if block
         sprintf(self.class::FORMAT_STRINGS[:single], *args)
       end
 
+      # Format multiple log messages.
+      #
+      # @param level [Symbol] Log level.
+      # @param label [String] Identifier, for instance logs.app.db.
+      # @param messages [Array<#to_s>] The actual messages.
+      #
+      # @return [String] The log message.
+      #
+      # @see `FORMAT_STRINGS[:header]`
+      #
+      # @api plugin.
+      #
+      # @todo
+      #   There's no documentation for the block yet, it's being
+      #   used only for extending functionality from the subclasses.
+      #   It should be probably refactored, otherwise it will get
+      #   proper documentation.
       def format_multiple_messages(level, label, messages, &block)
         args = [level.to_s.upcase, timestamp]
         args = block.call(*args) if block
@@ -26,21 +63,52 @@ module Logging
         messages.unshift(nil)
         header + messages.join("\n  ")
       end
+
+      protected
+
+      # @api protected
+      def timestamp
+        Time.now.strftime('%H:%M:%S')
+      end
     end
 
+    # This is useful for logging on console.
     class JustMessage < Default
-      # NOTE: Don't use ||=, otherwise it won't define it due its presence in superclass.
+      # The `single` key is used by {#format_single_message},
+      # whereas `header` is used by {#format_multiple_messages}.
       FORMAT_STRINGS = {
         single: "~ %s",
         header: "~ %s"
       }
 
+      # Format single log message.
+      #
+      # @param level [Symbol] Log level.
+      # @param label [String] Identifier, for instance logs.app.db.
+      # @param message [#to_s] The actual log message.
+      #
+      # @return [String] The log message.
+      #
+      # @see `FORMAT_STRINGS[:single]`
+      #
+      # @api plugin.
       def format_single_message(level, label, message)
         super(level, label, message) do |*args|
           [args.last]
         end
       end
 
+      # Format multiple log messages.
+      #
+      # @param level [Symbol] Log level.
+      # @param label [String] Identifier, for instance logs.app.db.
+      # @param messages [Array<#to_s>] The actual messages.
+      #
+      # @return [String] The log message.
+      #
+      # @see `FORMAT_STRINGS[:header]`
+      #
+      # @api plugin.
       def format_multiple_messages(level, label, messages)
         super(level, label, messages) do |*args|
           [args.last]
@@ -49,20 +117,44 @@ module Logging
     end
 
     class Colourful < Default
+      # Colours for each log level.
       LEVELS ||= {error: "\033[31m", warn: "\033[33m", info: "\033[36m"}
 
-      # NOTE: Don't use ||=, otherwise it won't define it due its presence in superclass.
+      # The `single` key is used by {#format_single_message},
+      # whereas `header` is used by {#format_multiple_messages}.
       FORMAT_STRINGS = {
         single: "%s%-5s \033[37m%s\033[0m -- %s",
         header: "%s%-5s \033[37m%s\033[0m"
       }
 
+      # Format single log message.
+      #
+      # @param level [Symbol] Log level.
+      # @param label [String] Identifier, for instance logs.app.db.
+      # @param message [#to_s] The actual log message.
+      #
+      # @return [String] The log message.
+      #
+      # @see `FORMAT_STRINGS[:single]`
+      #
+      # @api plugin.
       def format_single_message(level, label, message)
         super(level, label, message) do |*args|
           args.unshift(LEVELS[level])
         end
       end
 
+      # Format multiple log messages.
+      #
+      # @param level [Symbol] Log level.
+      # @param label [String] Identifier, for instance logs.app.db.
+      # @param messages [Array<#to_s>] The actual messages.
+      #
+      # @return [String] The log message.
+      #
+      # @see `FORMAT_STRINGS[:header]`
+      #
+      # @api plugin.
       def format_multiple_messages(level, label, messages)
         super(level, label, messages) do |*args|
           args.unshift(LEVELS[level])
--- spec/logging/formatters_spec.rb
+++ spec/logging/formatters_spec.rb
@@ -2,5 +2,134 @@
 
 require 'spec_helper'
 
-describe Logging::Logger do
+describe Logging::Formatters::Default do
+  describe '#format_single_message' do
+    let(:message) do
+      subject.format_single_message(:info, 'logs.test.app', "Hello World!")
+    end
+
+    it "should combine identifier with log level" do
+      pending "it's not including the log level yet" do
+        message.should match('logs.test.app.info')
+      end
+    end
+
+    it "should display timestamp" do
+      message.should match(/\d{2}:\d{2}:\d{2}/)
+    end
+
+    it "should display the actual message" do
+      message.should match("Hello World!")
+    end
+  end
+
+  describe '#format_multiple_messages' do
+    let(:message) do
+      subject.format_multiple_messages(:info, 'logs.test.app', ["Hello", "World!"])
+    end
+
+    it "should combine identifier with log level" do
+      pending "it's not including the log level yet" do
+        message.should match('logs.test.app.info')
+      end
+    end
+
+    it "should display timestamp" do
+      message.should match(/\d{2}:\d{2}:\d{2}/)
+    end
+
+    it "should display the actual message" do
+      message.should match("Hello\n  World!")
+    end
+  end
+end
+
+describe Logging::Formatters::JustMessage do
+  describe '#format_single_message' do
+    let(:message) do
+      subject.format_single_message(:info, 'logs.test.app', "Hello World!")
+    end
+
+    it "should not display the identifier" do
+      message.should_not match('logs.test.app')
+    end
+
+    it "should not display the log level" do
+      message.should_not match('info')
+    end
+
+    it "should not display timestamp" do
+      message.should_not match(/\d{2}:\d{2}:\d{2}/)
+    end
+
+    it "should display the actual message" do
+      message.should eql("~ Hello World!")
+    end
+  end
+
+  describe '#format_multiple_messages' do
+    let(:message) do
+      subject.format_multiple_messages(:info, 'logs.test.app', ["Hello", "World!"])
+    end
+
+    it "should not display the identifier" do
+      message.should_not match('logs.test.app')
+    end
+
+    it "should not display the log level" do
+      message.should_not match('info')
+    end
+
+    it "should not display timestamp" do
+      pending "This obviously doesn't work now" do
+        message.should_not match(/\d{2}:\d{2}:\d{2}/)
+      end
+    end
+
+    it "should display the actual message" do
+      message.should match("Hello\n  World!")
+    end
+  end
+end
+
+describe Logging::Formatters::Colourful do
+  describe '#format_single_message' do
+    let(:message) do
+      subject.format_single_message(:info, 'logs.test.app', "Hello World!")
+    end
+
+    it "should combine identifier with log level" do
+      pending "it's not including the log level yet" do
+        message.should match('logs.test.app.info')
+      end
+    end
+
+    it "should display timestamp" do
+      message.should match(/\d{2}:\d{2}:\d{2}/)
+    end
+
+    it "should display the actual message" do
+      message.should match("Hello World!")
+    end
+  end
+
+  describe '#format_multiple_messages' do
+    let(:message) do
+      subject.format_multiple_messages(:info, 'logs.test.app', ["Hello", "World!"])
+    end
+
+    it "should combine identifier with log level" do
+      pending "it's not including the log level yet" do
+        message.should match('logs.test.app.info')
+      end
+    end
+
+    it "should display timestamp" do
+      message.should match(/\d{2}:\d{2}:\d{2}/)
+    end
+
+    it "should display the actual message" do
+      message.should match("Hello\n  World!")
+    end
+  end
 end
